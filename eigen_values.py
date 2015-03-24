76520672fd6b9ab9a6f54fae79909fb27d41f9c0
Artem Kalikin <ak7613@ray23.doc.ic.ac.uk>
2015-03-24 18:54:08 +0000

    added comments - final version

--- eigen_values.py
+++ eigen_values.py
@@ -1,57 +1,87 @@
-import numpy as np
+#credit - Artem Kalikin (ak7613@ic.ac.uk)
+#Performs QR iterations using Gram-Schmidt Process
+#until eigen values converge within a threshold
+#prints eigenvalues rounded to 3 (PRECISION) decimal points
 
-THRESHOLD = 0.001
+"""
+input file format
+first line - matrix dimension
+then numbers divided by space
+ex:
+4
+1 2 3 4
+3 2 1 4
+2 3 1 4
+4 3 1 2
+"""
+
+import numpy as np
+THRESHOLD = 0.001 #determines convergence
+PRECISION = 3   #number of decimal points
 
 def main():
-    file = open("in_matrix", "r")
-    size = int(file.readline().strip("\n"))
-    lines = file.read().split("\n")
-    a = np.zeros(shape=(size,size))
-    for i in range(size):
-        a[i] = map(float, lines[i].split(" "))
+    a = read_file("in_matrix")
     print("Input Matrix")
     print(a)
     e = eigen(a)
     print("Eigen Values")
     print(e)
 
-def gs(inp):
+#Opens file and fills the matrix with values from the file
+#if file does not exist uses predefined matrix
+def read_file(filename):
+    try:
+        file = open(filename)
+        size = int(file.readline().strip("\n"))
+        lines = file.read().split("\n")
+        a = np.zeros(shape=(size,size))
+        for i in range(size):
+            a[i] = map(float, lines[i].split(" "))
+    except IOError:
+        print "Could not open file, using standard input matrix"
+        a = [[1,2,3,4], [3,2,1,4], [2,3,1,4], [4,3,1,2]]
+    return a
+
+def gram_schmidt(inp):
     r = np.zeros(shape=(len(inp), len(inp)))
     q = np.zeros(shape=(len(inp), len(inp)))
-    u = inp
-    for k in range(len(u)):
-        r[k][k] = sum([u[i][k]**2 for i in range(len(u))])**0.5 #magnitude : sqrt of sum of squares
-        for i in range(len(u)):
-            q[i][k] = u[i][k] / r[k][k]
-        for j in range(k+1, len(u)):
-            r[k][j] = sum([q[i][k] * u[i][j] for i in range(len(u))])
-            for i in range(len(u)):
-                u[i][j] -= r[k][j] * q[i][k]
+    for k in range(len(inp)):
+        r[k][k] = sum([inp[i][k]**2 for i in range(len(inp))])**0.5 #magnitude : sqrt of sum of squares
+        for i in range(len(inp)):
+            q[i][k] = inp[i][k] / r[k][k]
+        for j in range(k+1, len(inp)):
+            r[k][j] = sum([q[i][k] * inp[i][j] for i in range(len(inp))])
+            for i in range(len(inp)):
+                inp[i][j] -= r[k][j] * q[i][k]
     return q,r
 
 def iteration(q,r):
     return np.dot(r, q)
 
-def eigen(u):
-    prev = np.zeros(shape=(len(u), len(u)))
-    i = 0
-    while(i < 50 and not diagonal(u, prev)):
-        prev = np.copy(u)
-        i+=1
-        q,r = gs(u)
-        u = iteration(q,r)
-    return values(u)
+#QR Iterations, If metrix does not converge - stops after 50 iterations
+def eigen(inp):
+    inp_prev = np.zeros(shape=(len(inp), len(inp)))
+    iter = 0
+    while iter < 50 and not diagonal(inp, inp_prev):
+        inp_prev = np.copy(inp)
+        iter+=1
+        q,r = gram_schmidt(inp)
+        inp = iteration(q,r)
+    return values(inp)
 
-def diagonal(u, prev):
-    for i in range(len(u)):
-        if abs(u[i][i] - prev[i][i]) > THRESHOLD:
+#Checks for convergence, if difference in diagonal
+# is less than THRESHOLD returns True
+def diagonal(inp, inp_prev):
+    for i in range(len(inp)):
+        if abs(inp[i][i] - inp_prev[i][i]) > THRESHOLD:
             return False
     return True
 
+#Returns eigen values - diagonal of the matrix
 def values(matrix):
     res = np.zeros(shape=(len(matrix)))
     for i in range(len(matrix)):
-        res[i] = matrix[i][i]
+        res[i] = round(matrix[i][i], PRECISION)
     return res
 
 if __name__ == '__main__':
